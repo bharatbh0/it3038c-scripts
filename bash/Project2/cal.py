5a0e0c4fd269ac77c030da77f4c5128c1ef71f3a
bharatbh0 <122412582+bharatbh0@users.noreply.github.com>
2023-03-27 21:19:08 -0400

    Update cal.py

--- bash/Project2/cal.py
+++ bash/Project2/cal.py
@@ -5,45 +5,43 @@ while True:
     print("3 for Multiplication")
     print("4 for Division")
     print("5 for Exponentiation")
+    print("6 for Quit")
 
-    choice= input("Enter your choice from above (1-5): ")
+    choice = input("Enter your choice from above (1-6): ")
 
-    #int only lets the user use whole number
+    if choice == "6":
+        print("Shutting down...")
+        break
+
+    # int only lets the user use whole number
     num1 = int(input("Enter your First choice: "))
     num2 = int(input("Enter your Second choice: "))
 
-    #Check the users choice 
+    # Check the users choice
     if choice == "1":
-        print(num1,"+", num2,)
+        print(num1, "+", num2,)
         print("Answer is", (num1+num2))
 
     elif choice == "2":
-        print(num1,"-", num2)
+        print(num1, "-", num2)
         print("Answer is", (num1-num2))
 
     elif choice == "3":
-        print(num1,"*", num2)
+        print(num1, "*", num2)
         print("Answer is", (num1*num2))
 
     elif choice == "4":
-        if num2 == 0.0:     #Can't divide by 0
+        if num2 == 0.0:  # Can't divide by 0
             print("Divide by 0 Error")
         else:
-            print(num1,"/", num2)
+            print(num1, "/", num2)
             print("Answer is", (num1/num2))
 
     elif choice == "5":
-        print("{} ** {} ={}". format(num1,num2,num1**num2))
+        print("{} ** {} ={}". format(num1, num2, num1**num2))
         print("Answer is", (num1**num2))
 
     else:
         print("Invalded Choice: Try again")
-    a = input("continue? (Y/N): ")
-    if a == "Y":
-        choice()
-    elif a == "N":
-        print("Shutting down...")
-        break
-    else:
-        print("Invalied input")
-        break
+
+    a = input("Tap Enter to continue:")
