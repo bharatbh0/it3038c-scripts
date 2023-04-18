6cc055caeb02d012cec91889139ed915161ba4eb
bharatbh0 <122412582+bharatbh0@users.noreply.github.com>
2023-04-18 19:06:54 -0400

    Update calculator.py

--- FinalProject/calculator.py
+++ FinalProject/calculator.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+##function calculate is define to perfrom basic operations here only +, -, *, and /
 def calculate(num1, num2, operator):
     if operator == '+':
         return num1 + num2
@@ -12,10 +13,12 @@ def calculate(num1, num2, operator):
     elif operator == '/':
         return num1 / num2
 
+##Send the user to homepage 
 @app.route('/')
 def home():
     return render_template('calculate.html')
 
+##Once the user chooses the choices result then excuates showing the answer
 @app.route('/result', methods=['POST'])
 def result():
     num1 = int(request.form['num1'])
@@ -24,6 +27,7 @@ def result():
     result = calculate(num1, num2, operator)
     return render_template('result.html', result=result)
 
+##Checks if the current module is the main module and runs the application with debug enabled 
 if __name__ == '__main__':
     app.run(debug=True)
 
